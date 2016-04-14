e9db0532e48966850a41f7536dc9bcf3b651716c
cknight8 <361532193@qq.com>
2016-04-14 08:37:32 +0200

    Added files via upload

---
+++ lab2.java
@@ -0,0 +1,14 @@
+package se_lab2;
+
+
+public class lab2 {
+	public static int n = 10;
+	
+	public static void main(String[] args) {
+		for (int i = 1; i <= n; i++){
+			System.out.println(i);
+		}
+		System.out.println("Hello GitHub!");
+	}
+
+}
